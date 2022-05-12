31f25b9fd0e06f22e5a3b58bac2d6c4e92ec264e
lrodriguez911 <lrodriguez.arevalosc@gmail.com>
2022-05-12 13:12:13 -0300

    2 files modified

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-class Counter extends React.Component {
+class Contador extends React.Component {
   state = {
-    counter: 0
+    conteo: 0
   }
   increment = () => {
-    this.setState(() => ({counter: this.state.counter+1}));
+    this.setState(() => ({conteo: this.state.conteo+1}));
   }
   render() {
     return <div className="div">
-    <h1><p>{this.state.counter}</p></h1>
+    <h1><p>{this.state.conteo}</p></h1>
     <button onClick={this.increment}>Increment</button>
     </div>;
   }
 }
 
-const el = <Counter />; 
+const count = <Contador />; 
 ReactDOM.render(
-  el, 
+  count, 
   document.getElementById('root')
 );
